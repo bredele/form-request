f20d4c37ee1c2838e4c9d009c5608c8dd8e87565
bredele <olivier.wietrich@gmail.com>
2016-07-02 14:57:35 -0600

    encore url encoded

--- index.js
+++ index.js
@@ -12,13 +12,33 @@
 
 module.exports = function(form, cb) {
   var data = new FormData(form)
+  var type = form.enctype;
   form.addEventListener('submit', function(event) {
     var request = new XMLHttpRequest
     request.addEventListener('load', cb)
     request.addEventListener('error', cb)
     request.open(form.method, form.action)
-    request.send(data)
+    request.send(encode(data, type))
     event.preventDefault()
   })
   return data
+}
+
+/**
+ * Encode data according form enctype.
+ *
+ * @param {FormData} data
+ * @param {String} type
+ * @api private
+ */
+
+function encode(data, type) {
+  if(type === 'application/x-www-form-urlencoded') {
+    var query = ''
+    for(var entry of data.entries()) {
+      query += entry[0] + '=' + encoreURI(entry[1]) + '&'
+    }
+    return query
+  }
+  return data;
 }
